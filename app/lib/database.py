c16568a5b36eff679b51d9ab13bbbfaeeed5d965
Sean O'Connor <so@seano.dev>
2023-06-04 16:48:39 -0600

    Create connection to MongoDB

---
+++ app/lib/database.py
@@ -0,0 +1,11 @@
+
+import motor.motor_asyncio
+
+from app.config import config
+
+_connection_uri = config['MONGO_CONNECTION_URI']
+
+client = motor.motor_asyncio.AsyncIOMotorClient(_connection_uri)
+
+database = client['upcomingmcu']
+collection = database['productions']
