7b41381949345227481ef34c5008b7f9b029725f
Sean O'Connor <so@seano.dev>
2023-06-04 16:58:00 -0600

    Create root route

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
+import random
+
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 from app.errors import (exception_handler, http_exception_handler,
                         value_error_handler)
@@ -20,3 +23,10 @@ app.add_exception_handler(ValueError, value_error_handler)
 app.add_exception_handler(Exception, exception_handler)
 
 app.include_router(v1.router)
+
+
+@app.get('/')
+async def root():
+    quotes = ["I love you 3000.", "I am Iron Man.",
+              "Avengers...assemble.", "I am Groot.", "Hail Hydra."]
+    return JSONResponse(random.choice(quotes), media_type='text/plain')
