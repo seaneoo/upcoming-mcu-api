cd11c2bfb87c545a88d016b7ff00130603b70cd4
Sean O'Connor <so@seano.dev>
2023-06-05 09:08:37 -0600

    Add certifi to AsyncIOMotorClient

--- app/lib/database.py
+++ app/lib/database.py
@@ -1,11 +1,13 @@
 
+import certifi
 import motor.motor_asyncio
 
 from config import config
 
 _connection_uri = config['MONGO_CONNECTION_URI']
 
-client = motor.motor_asyncio.AsyncIOMotorClient(_connection_uri)
+client = motor.motor_asyncio.AsyncIOMotorClient(
+    _connection_uri, tlsCAFile=certifi.where())
 
 database = client['upcomingmcu']
 collection = database['productions']
