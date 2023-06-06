03237c489b28da131145a65ed99a399c6b05a33d
Sean O'Connor <so@seano.dev>
2023-06-06 15:23:59 -0600

    Redirect `/` to `/docs`

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
-import random
-
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
+from fastapi.responses import RedirectResponse
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 
 from app.errors import (exception_handler, http_exception_handler,
                         value_error_handler)
-from app.limiter import RATE_LIMIT, limiter
+from app.limiter import limiter
 from app.router import v1
 
 app = FastAPI()
@@ -29,9 +27,6 @@ app.add_exception_handler(Exception, exception_handler)
 app.include_router(v1.router)
 
 
-@app.get('/')
-@limiter.limit(RATE_LIMIT)
-async def root(request: Request):
-    quotes = ["I love you 3000.", "I am Iron Man.",
-              "Avengers...assemble.", "I am Groot.", "Hail Hydra."]
-    return JSONResponse(random.choice(quotes), media_type='text/plain')
+@app.get('/', include_in_schema=False)
+async def docs_redirect():
+    return RedirectResponse(url='/docs')
